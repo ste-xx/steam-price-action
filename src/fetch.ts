72e8c452d323cc2781ca411274798313444e91ac
stefanbreitenstein <stefan.breitenstein@tt-s.com>
2022-04-20 07:50:21 +0200

    adjust code

--- src/fetch.ts
+++ src/fetch.ts
@@ -18,47 +18,44 @@ interface FetchResponse {
   }[]
 }
 
+type Entry = [label: string, price: number, url: string]
+
 export interface Args {
   readInput: () => Input
   fetchData: (input: Input[number]) => Promise<FetchResponse>
-  writeOutput: (
-    header: ['label', 'price', 'link'],
-    data: [label: string, price: number, url: string][]
-  ) => void
+  writeOutput: (header: ['label', 'price', 'link'], data: Entry[]) => void
 }
 
+const addArg =
+  <T, U>(fn: (arg: T) => Promise<U>) =>
+  async (arg: T): Promise<[U, T]> =>
+    [await fn(arg), arg]
+
 export const fetch = async (args: Args): Promise<void> => {
+  const fetchDataWithArgs = addArg(args.fetchData)
+
   const responseInputTuples = await Promise.all(
-    args
-      .readInput()
-      .map(
-        async (input): Promise<[FetchResponse, Input[number]]> => [
-          await args.fetchData(input),
-          input
-        ]
-      )
+    args.readInput().map(fetchDataWithArgs)
   )
 
-  const result = responseInputTuples.map(
-    ([response, input]): [string, number, string] => {
-      const MERCHANT_G2A = '61'
-      const offer = response.offers.find(
-        o => o.merchant === MERCHANT_G2A && o.platform === 'steam'
-      )
-      return [
-        input.label,
-        offer?.price.eur.priceWithoutCoupon ?? offer?.price.eur.price ?? -1,
-        offer?.affiliateUrl ?? '-'
-      ]
-    }
-  )
+  const result = responseInputTuples.map(([response, input]): Entry => {
+    const MERCHANT_G2A = '61'
+    const offer = response.offers.find(
+      o => o.merchant === MERCHANT_G2A && o.platform === 'steam'
+    )
+    return [
+      input.label,
+      offer?.price.eur.priceWithoutCoupon ?? offer?.price.eur.price ?? -1,
+      offer?.affiliateUrl ?? '-'
+    ]
+  })
 
   args.writeOutput(['label', 'price', 'link'], result)
 }
 
 export const toTsv = (
   header: ['label', 'price', 'link'],
-  data: [string, number, string][]
+  data: Entry[]
 ): string => {
   const rows = data.map(e => e.join('\t'))
   return [header.join('\t'), ...rows].join('\n')
